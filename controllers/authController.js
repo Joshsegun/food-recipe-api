ea93e86538f132a666a7935127fba0620ea5eadf
Oluwole Olusegun Joshua <oluwoleolusegun14@gmail.com>
2025-01-04 23:55:02 +0100

    Working on forgot password

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,8 @@ const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 const asyncAwaitHandler = require("../utils/asyncAwaitHandler");
 const ErrorHandler = require("../utils/errorHandler");
+const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
 
 exports.register = asyncAwaitHandler(async (req, res) => {
   const newUser = await User.create({
@@ -55,4 +57,49 @@ exports.login = asyncAwaitHandler(async (req, res, next) => {
     user,
     token,
   });
+});
+
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+exports.forgotPassword = asyncAwaitHandler(async (req, res, next) => {
+  //GET User based on email
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) return next(new ErrorHandler("User not found", 404));
+
+  //Generate a reset token
+  const resetToken = jwt.sign({ id: user._id }, process.env.JWT_RESET_SECRET, {
+    expiresIn: "1h",
+  });
+
+  const resetLink = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/users/resetPassword/${resetToken}`;
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: "Password Reset Request",
+    html: `<p>You requested a password reset. Click <a href="${resetLink}">here</a> to reset your password. The link expires in 1 hour.</p>`,
+  };
+
+  await transporter.sendMail(mailOptions);
+
+  res.status(200).json({
+    status: "success",
+    message: "Password reset link sent to your email",
+  });
+
+  // return next(
+  //   new AppError('There was an error sending the email. Try again later!'),
+  //   500
+  // );
 });
